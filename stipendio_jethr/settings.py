9fa80c338a37aa1f23acd4ae831d107b31f7e588
mrheineken121 <leocop.14@gmail.com>
2025-11-29 19:57:10 +0100

    fix

--- stipendio_jethr/settings.py
+++ stipendio_jethr/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,12 +21,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-n%@cm2&3bo*fp1p^jtwj2cc&exxyh)(#j!&)!x@^b6a5_2$44%'
+SECRET_KEY = os.environ.get("SECRET_KEY", "chiave-solo-per-sviluppo")
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
@@ -116,7 +118,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = "/static/"
-STATICFILES_DIRS = [BASE_DIR / "stipendio_jethr" / "static"]
+STATIC_ROOT = BASE_DIR / "staticfiles"
+#STATICFILES_DIRS = [BASE_DIR / "stipendio_jethr" / "static"]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
